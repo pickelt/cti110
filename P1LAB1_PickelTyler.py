16e72cd2f9775635c8ec2fc0f6fafbd645deec8d
pickelt <160452501+pickelt@users.noreply.github.com>
2024-02-18 23:27:26 -0500

    Add files via upload
    First submission

---
+++ P1LAB1_PickelTyler.py
@@ -0,0 +1,15 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Tyler Pickel
+... # 2/18/2024
+... # P1LAB1
+... # First name is the input and outputs a welcome to that name.
+... 
+... # This program takes a first name as input and outputs a welcome message to that name.
+... 
+... def welcome_message():
+...     name = input("Tyler")
+...     print(f"Hey {Tyler}")
+...     print("Welcome to zyBooks!")
+... 
+... welcome_message()
